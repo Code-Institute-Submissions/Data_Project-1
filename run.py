696ef617a6c5bf617bf0847b543cfbe1d47ab18a
Geoff Doig <geoff.doig71@gmail.com>
2019-02-07 17:26:19 +0000

    Added views/routes for sign in and search pages

--- run.py
+++ run.py
@@ -1,17 +1,31 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, session
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+app.secret_key = os.getenv("SECRET", "secret123string")
 app.config["MONGO_URI"] = "mongodb://root:R00tUser@ds249128.mlab.com:49128/data_project"
 app.config["MONGO_DBNAME"] = "data_project"
 
 mongo = PyMongo(app)
 
-@app.route('/')
+@app.route('/', methods = ["GET", "POST"])
 def index():
+    """Welcome / Sign in page"""
+    if request.method == "POST":
+        session["username"] = request.form["username"]
+    if "username" in session:
+        return redirect(url_for("search", username = session["username"]))
+    return render_template("index.html")
+    
+@app.route('/search')
+def search():
+    return render_template("search.html")
+    
+@app.route('/recipe')
+def recipe():
+    """Display an individual recipe"""
     return render_template("recipes.html", recipes = mongo.db.recipes.find())
-
 
 
 if __name__ == '__main__':
