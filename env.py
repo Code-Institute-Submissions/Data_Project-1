9778d2d27f04a862ac356727ee1b75e21ee6ef48
Geoff Doig <geoff.doig71@gmail.com>
2019-02-11 15:54:12 +0000

    Added env.py for security, created working add recipe functionality

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ["MONGO_URI"]= "mongodb://root:R00tUser@ds249128.mlab.com:49128/data_project"
+os.environ["MONGO_DBNAME"]= "data_project"
--- run.py
+++ run.py
@@ -1,11 +1,13 @@
 import os
+import env
 from flask import Flask, render_template, redirect, request, url_for, session
 from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 app.secret_key = os.getenv("SECRET", "secret123string")
-app.config["MONGO_URI"] = "mongodb://root:R00tUser@ds249128.mlab.com:49128/data_project"
-app.config["MONGO_DBNAME"] = "data_project"
+app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+app.config["MONGO_DBNAME"] = os.getenv("MONGO_DBNAME")
 
 mongo = PyMongo(app)
 
@@ -21,14 +23,46 @@ def index():
 @app.route('/search')
 def search():
     return render_template("search.html")
+
+@app.route('/add_recipe')
+def add_recipe():
+    return render_template("addrecipe.html")
     
+@app.route('/insert_recipe', methods = ["POST"])
+def insert_recipe():
+    ingredientsList = []
+    ingredientItems = request.form.getlist("ingredient_name")
+    ingredientQty = request.form.getlist("quantity")
+    i = 0
+    while i < len(ingredientItems):
+        ingredient = {"ingredient_name": ingredientItems[i], "quantity": ingredientQty[i]}
+        ingredientsList.append(ingredient)
+        i +=1
+    if request.form.get("allergens") == "on":
+        allergens = True
+        allergen_name = request.form["allergen_name"]
+    else:
+        allergens = False
+        allergen_name = ""
+    new_recipe = {
+        "recipe_name": request.form["recipe_name"],
+        "style_name": request.form["style_name"],
+        "author_name": request.form["author_name"],
+        "ingredients": ingredientsList,
+        "instructions": request.form["instructions"],
+        "allergens": allergens,
+        "allergen_name": allergen_name,
+        "views": "0"
+    }
+    mongo.db.recipes.insert_one(new_recipe)
+    return redirect(url_for("search"))
+      
 @app.route('/recipe')
 def recipe():
     """Display an individual recipe"""
-    return render_template("recipes.html", recipes = mongo.db.recipes.find())
-
+    selected = mongo.db.recipes.find_one({"recipe_name": "test recipe"})
+    return render_template("recipes.html", recipe = selected)
 
 if __name__ == '__main__':
     app.run(host = os.getenv('IP'), port = int(os.getenv('PORT')), debug = True)
-
-
+    
